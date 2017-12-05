4c28942296560e718d39f36b27011ade092bfcfa
Marshall Houston <marshall.houston@gmail.com>
2017-12-05 14:10:11 -0700

    add stub_omniauth test method, test for logging in

---
+++ spec/features/user_can_login_with_github_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature "A visitor can login" do
+  scenario "using github credentials" do
+    stub_omniauth
+    visit root_path
+    expect(page).to have_link("Login with Github")
+    click_link "Login with Github"
+    expect(page).to have_content("marshallhouston")
+    expect(page).to have_link("Logout")
+  end
+end
--- spec/features/user_can_view_basic_information_spec.rb
+++ spec/features/user_can_view_basic_information_spec.rb
@@ -1,14 +1,37 @@
 require 'rails_helper'
 
-describe "As a logged in user" do
+describe "As an authenticated user" do
   describe "when I visit my profile page" do
-    it "I see the correct number of repositories" do
+    it "my user github information" do
 
       #create test object
       # omniauth.stub or Faker::Omniauth.github
 
+      # As an authenticated user,
+      # after I successfully log in,
+      # I should see my profile pic,
+      within(".profile_picture") do
+        expect(page).to have_css("img")
+      end
+      # repositories with number,
+      within(".repositories") do
+        expect(page).to have_content("Repositories 42")
+      end
+      # number of starred repos,
+      within(".starred_repos") do
+        expect(page).to have_content("Starred 6")
+      end
+      # followers, and
+      within(".followers") do
+        expect(page).to have_content("Followers 6")
+      end
+
+      # following
+      within(".following") do
+        expect(page).to have_content("Following 2")
+      end
+      
       visit "/#{user.username}" #will need to slug this
-      expect(page).to have_content("Repositories 42")
     end
   end
 end
