2618fecbf88ca51b00c5422399c810cd61c47ed3
Marshall Houston <marshall.houston@gmail.com>
2017-12-04 21:15:47 -0700

    add logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/auth/github', as: :github_login
   get '/auth/github/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
 
   resources :users, only: [:show]
 end
