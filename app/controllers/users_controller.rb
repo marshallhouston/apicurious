3fcaeb5ca17ad592a22d9b4ee29bdf70f01eb113
Marshall Houston <marshall.houston@gmail.com>
2017-12-04 22:17:15 -0700

    add failing feature test for user repository info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    current_user
+    user_info_request = Faraday.get("https://api.github.com/user?access_token=#{current_user.token}")
+    @parsed_user = JSON.parse(user_info_request.body)
   end
-  
+
 end
---
+++ spec/features/user_can_view_basic_information_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "As a logged in user" do
+  describe "when I visit my profile page" do
+    it "I see the correct number of repositories" do
+
+      #create test object
+      # omniauth.stub or Faker::Omniauth.github
+
+      visit "/#{user.username}"
+      expect(page).to have_content("Repositories 42")
+    end
+  end
+end
