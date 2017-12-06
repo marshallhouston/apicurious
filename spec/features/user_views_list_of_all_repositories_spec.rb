789915b36880716981c3ffdf5b363617c47c3d55
Marshall Houston <marshall.houston@gmail.com>
2017-12-05 21:28:32 -0700

    update test for more specific routing

--- spec/features/user_views_list_of_all_repositories_spec.rb
+++ spec/features/user_views_list_of_all_repositories_spec.rb
@@ -6,7 +6,10 @@ feature "As an authenticated user" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     github_user = GithubUser.new(stub_omniauth)
 
-    visit "/users/#{user.id}/repositories"
+    visit user_path(user)
+    click_on "Repositories"
+
+    expect(current_path).to eq(user_repositories_path(user))
 
     within ".repositories" do
       expect(page).to have_css(".repo_name")
