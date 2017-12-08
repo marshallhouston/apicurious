c35cc3a2dd203dd163babbbdd43ac428252b222b
Marshall Houston <marshall.houston@gmail.com>
2017-12-07 20:48:51 -0700

    unskip test for followers, update test

--- spec/features/user_views_list_of_followers_spec.rb
+++ spec/features/user_views_list_of_followers_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-xfeature "As an authenticated user" do
-  scenario "when I click on followers" do
+feature "As an authenticated user" do
+  context "when I click on followers" do
     it "I see a list of all followers" do
       VCR.use_cassette("user_views_list_of_followers") do
         stub_omniauth
+
+        visit root_path
+        click_on "Login with Github"
+
         click_on "Followers"
 
         within ".followers" do
           expect(page).to have_content("wiseemily88")
-          expect(page).to have_content("maxscores")
+          expect(page).to have_content("Maxscores")
           expect(page).to have_content("PlanetEfficacy")
         end
       end
