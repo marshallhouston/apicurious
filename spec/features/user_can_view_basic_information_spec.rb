eefb7eb0493d2d94fcb00ed09cc9ab3539751315
Marshall Houston <marshall.houston@gmail.com>
2017-12-07 23:48:08 -0700

    add more targeted capybara selector

--- spec/features/user_can_view_basic_information_spec.rb
+++ spec/features/user_can_view_basic_information_spec.rb
@@ -11,20 +11,23 @@ describe "As an authenticated user" do
         visit root_path
 
         click_link "Login with Github"
-        within(".repositories") do
-          expect(page).to have_content("Repositories 42")
-        end
-        # number of starred repos,
-        within(".starred_repos") do
-          expect(page).to have_content("Starred 6")
-        end
-        # followers, and
-        within(".followers") do
-          expect(page).to have_content("Followers 3")
-        end
-        # following
-        within(".following") do
-          expect(page).to have_content("Following 6")
+
+        within(".info-bar") do
+          within(".repositories") do
+            expect(page).to have_content("Repositories 42")
+          end
+          # number of starred repos,
+          within(".starred_repos") do
+            expect(page).to have_content("Starred 6")
+          end
+          # followers, and
+          within(".followers") do
+            expect(page).to have_content("Followers 3")
+          end
+          # following
+          within(".following") do
+            expect(page).to have_content("Following 6")
+          end
         end
       end
     end
